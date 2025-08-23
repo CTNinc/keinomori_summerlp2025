a579b8dc0ed4c3a68a304c85b8a9feb6034b05a3
Tsukada <daichi202405@gmail.com>
2025-08-24 01:39:38 +0900

    contact.phpのメール設定とその他の調整

--- contact.php
+++ contact.php
@@ -99,8 +99,8 @@ if (!empty($errors)) {
 }
 
 // メール送信処理
-$to = 'k-keinomori@keinomori.com'; // 会社宛てのメールアドレス
-$cc = 'info@keinomori.com'; // CC
+$to = 'keinomori@keinomori.com'; // 会社宛てのメールアドレス
+$cc = 'info@ctn-group.com'; // CC
 $subject = '軽の森 お問い合わせフォーム - ' . $car_type;
 
 // メールヘッダー設定
